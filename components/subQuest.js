ac54d79a40dda4a2dd5c6d970d974b63c04796d9
Bert Allen <mrlaughingdragon@aol.com>
2016-06-29 13:31:42 -0600

    putting bits together (slowly)

---
+++ components/subQuest.js
@@ -0,0 +1,9 @@
+(function () {
+    angular.module("MasterController")
+        .component("SubQuestion", {
+            templateURL: "subQuestMod.html",
+            controller: function () {
+
+            }
+        });
+} ())
---
+++ components/viewQueue.js
@@ -0,0 +1,9 @@
+(function () {
+    angular.module("MasterController")
+        .component("ViewQueue", {
+            templateURL: "viewQueueMod.html",
+            controller: function () {
+
+            }
+        });
+} ())
