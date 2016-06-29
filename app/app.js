b7e2647e99cb47b9a346278bc645595605b2f476
Bert Allen <mrlaughingdragon@aol.com>
2016-06-29 16:39:46 -0600

    pointing front page buttons to a switch-case to redirect to a component

--- app/app.js
+++ app/app.js
@@ -1,9 +1,19 @@
 (function () {
     angular.module("MasterController", [])
         .controller("QueueController", function () {
-        
-        
+            var qc = this
+            qc.openModel = function (mod) {
+                switch (mod) {
+                    case 1: //submit question
 
-    })
+                        break;
+                    case 2: //inspect queue
 
-}())
+                        break;
+                }
+            }
+
+
+        })
+
+} ())
